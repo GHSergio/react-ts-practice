f8cd294072ec47f604889a6a1dce5fc88a0b06f0
Ming Hsu <n209202003@gmail.com>
2024-08-08 09:11:00 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { ThemeProviderComponent } from "./contexts/ThemeContext";
 import { MovieProvider } from "./contexts/MovieContext";
 import Main from "./Main";
-import "./styles/index.scss";
 
 function App() {
   return (
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: "localhost",
-        port: 3000,
-        open: true,
-    },
-    build: {
-        outDir: "dist",
-    },
+  plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+  },
 });
